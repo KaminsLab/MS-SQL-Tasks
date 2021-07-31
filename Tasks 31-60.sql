4863d4d02e70d647cfe5580c343d216f9686ff03
KaminsLab <55655435+KaminsLab@users.noreply.github.com>
2021-07-31 21:31:35 +0300

    Added tasks 52-57

--- Tasks 31-60.sql
+++ Tasks 31-60.sql
@@ -132,7 +132,7 @@ WHERE bore = 16
 
 --50. Find the battles in which Kongo-class ships from the Ships table were engaged.
 SELECT DISTINCT battle FROM Outcomes
-WHERE Outcomes.ship IN (SELECT [name] FROM Ships WHERE Ships.class = 'Kongo')
+WHERE Outcomes.ship IN (SELECT [name] FROM Ships WHERE Ships.class = 'Kongo');
 
 --51. Find the names of the ships with the largest number of guns among all ships having the same displacement 
 --(including ships in the Outcomes table). 
@@ -149,3 +149,65 @@ SELECT
       WHERE ci.displacement=c.displacement
         and ci.class in (SELECT sh.class FROM sh)
     )
+
+--52. Determine the names of all ships in the Ships table that can be a Japanese battleship 
+--having at least nine main guns with a caliber of less than 19 inches 
+--and a displacement of not more than 65 000 tons.
+SELECT [name] FROM Ships sh LEFT JOIN Classes cl ON sh.class = cl.class
+WHERE [type] = 'bb' AND (numGuns >= 9 OR numGuns IS NULL)
+AND (bore < 19 OR bore IS NULL) 
+AND (displacement <= 65000 OR displacement IS NULL)
+AND country = 'Japan'
+
+--53. With a precision of two decimal places, determine the average number of guns for the battleship classes. 
+SELECT CAST(AVG(CAST(numGuns AS decimal(18,2))) AS decimal(18,2)) FROM Classes
+WHERE [type] = 'bb'
+
+--54. With a precision of two decimal places, determine the average number of guns 
+--for all battleships (including the ones in the Outcomes table). 
+SELECT CAST(AVG(CAST(numGuns AS decimal(18,2))) AS decimal(18,2)) FROM
+(SELECT [name] AS ship, numGuns FROM Classes
+INNER JOIN Ships ON Classes.class = Ships.class
+WHERE [type] = 'bb'
+UNION
+SELECT ship, numGuns FROM Classes
+INNER JOIN Outcomes ON Classes.class = Outcomes.ship
+WHERE [type] = 'bb') AS T
+
+--55.For each class, determine the year the first ship of this class was launched.
+--If the lead ship’s year of launch is not known, get the minimum year of launch for the ships of this class.
+--Result set: class, year.
+SELECT Classes.class, MIN(launched) FROM Classes LEFT JOIN Ships ON Classes.class = Ships.class
+GROUP BY Classes.class;
+
+--56.For each class, find out the number of ships of this class that were sunk in battles.
+--Result set: class, number of ships sunk.
+WITH all_ships_CTE AS (
+SELECT cl.class, [name] AS ship FROM Classes cl LEFT JOIN Ships sh ON cl.class = sh.class
+UNION
+SELECT cl.class, ship FROM Classes cl INNER JOIN Outcomes oc ON cl.class = oc.ship
+)
+
+SELECT class, SUM(CASE WHEN result='sunk' THEN 1 ELSE 0 END) AS sunk_ships FROM all_ships_CTE AS sh 
+LEFT JOIN Outcomes ON sh.ship = Outcomes.ship
+GROUP BY class;
+
+--57. For classes having irreparable combat losses and at least three ships in the database, display the name of the class and the number of ships sunk.
+WITH all_ships_CTE AS (
+SELECT cl.class, [name] AS ship FROM Classes cl LEFT JOIN Ships sh ON cl.class = sh.class
+UNION
+SELECT cl.class, ship FROM Classes cl INNER JOIN Outcomes oc ON cl.class = oc.ship
+)
+
+SELECT class, SUM(CASE WHEN result='sunk' THEN 1 ELSE 0 END) AS sunk_ships FROM all_ships_CTE AS sh 
+LEFT JOIN Outcomes ON sh.ship = Outcomes.ship
+GROUP BY class
+HAVING SUM(CASE WHEN result='sunk' THEN 1 ELSE 0 END) > 0 
+	AND 
+	(
+		SELECT COUNT(ship) FROM all_ships_CTE AS sh2
+		WHERE sh2.class = sh.class
+	) >= 3
+
+--58.
+
